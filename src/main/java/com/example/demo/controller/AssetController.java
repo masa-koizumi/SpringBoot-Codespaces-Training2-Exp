58ab421032f0eed4aad0e10bb75f0126b56b6a48
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-19 17:55:53 +0900

    Update AssetController.java

--- src/main/java/com/example/demo/controller/AssetController.java
+++ src/main/java/com/example/demo/controller/AssetController.java
@@ -1,3 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.entity.Asset; // これが重要
+import com.example.demo.repository.AssetRepository; // これが重要
+
 @Controller
 @RequestMapping("/assets")
 public class AssetController {
