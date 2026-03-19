5b704eecce5827d46ff060629d71801870cdab80
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-19 17:47:09 +0900

    Update LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,3 +1,10 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model; // AssetControllerで必要
+import jakarta.servlet.http.HttpSession; // LoginControllerで必要
+
 @Controller
 public class LoginController {
 
