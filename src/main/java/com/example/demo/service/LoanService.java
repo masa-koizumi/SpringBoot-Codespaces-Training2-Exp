f83ad1553b9253bea0ef8ac61a06ab913353177a
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-21 14:54:34 +0900

    Update LoanService.java

--- src/main/java/com/example/demo/service/LoanService.java
+++ src/main/java/com/example/demo/service/LoanService.java
@@ -5,7 +5,48 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoanService {
 
-    public void borrow(Long assetId, Long userId) {
-        // ここを実装させる
+    @Autowired
+    private LoanRepository loanRepo;
+
+    @Autowired
+    private AssetRepository assetRepo;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    // 貸出
+    public void loan(Long assetId, Long userId) {
+
+        Asset asset = assetRepo.findById(assetId)
+                .orElseThrow(() -> new RuntimeException("資産が存在しません"));
+
+        if ("LOANED".equals(asset.getStatus())) {
+            throw new RuntimeException("貸出中のため貸出できません");
+        }
+
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("ユーザが存在しません"));
+
+        Loan loan = new Loan();
+        loan.setAsset(asset);
+        loan.setUser(user);
+
+        asset.setStatus("LOANED");
+
+        loanRepo.save(loan);
+        assetRepo.save(asset);
+    }
+
+    // 返却
+    public void returnAsset(Long loanId) {
+
+        Loan loan = loanRepo.findById(loanId)
+                .orElseThrow(() -> new RuntimeException("Loanが存在しません"));
+
+        Asset asset = loan.getAsset();
+        asset.setStatus("AVAILABLE");
+
+        loanRepo.delete(loan);
+        assetRepo.save(asset);
     }
 }
