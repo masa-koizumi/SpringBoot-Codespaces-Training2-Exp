91340872b2f756d8a19819bd4616c347ce235903
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-21 15:12:52 +0900

    Update LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,28 +1,30 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model; // AssetControllerで必要
-import jakarta.servlet.http.HttpSession; // LoginControllerで必要
+import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.LoginService;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private LoginService service;
+
     @GetMapping("/")
-    public String loginPage() {
+    public String index() {
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(String name, HttpSession session) {
-        session.setAttribute("user", name);
-        return "redirect:/menu";
-    }
+    public String login(@RequestParam String name, HttpSession session) {
 
-    @GetMapping("/menu")
-    public String menu() {
-        return "menu";
+        User user = service.login(name);
+        session.setAttribute("user", user);
+
+        return "redirect:/menu";
     }
 }
