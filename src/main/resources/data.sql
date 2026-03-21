d9ec61e669f43ed0a7810aa3a4c99f918c6724dc
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-21 23:00:10 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Bob');
+-- パスワードカラムがあることを確認して実行
+INSERT INTO user_table (name, password) VALUES ('Alice', 'a123');
+INSERT INTO user_table (name, password) VALUES ('Bob', 'b456');
 
 INSERT INTO assets (name, status) VALUES ('Laptop', 'AVAILABLE');
 INSERT INTO assets (name, status) VALUES ('iPad', 'AVAILABLE');
