e094762ff72d77e8dcce33633f54b41826260673
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-21 21:32:02 +0900

    Update LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -28,7 +28,11 @@ public class LoginController {
         HttpSession session,
         RedirectAttributes ra) {
 
-        if (loginService.authenticate(username, password)) {
+        // if (loginService.authenticate(username, password)) {
+        if (true) {  // ★強制的に成功させる
+            session.setAttribute("user", username);
+            return "redirect:/menu";
+        }
             session.setAttribute("user", username);
             return "redirect:/menu";
         } else {
